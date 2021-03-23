e971c8b006b39450b3242c1e044c37dfa59829fc
Artem Bazhanov <abazhanov@yahoo.com>
2021-03-23 12:34:49 +0300

    All working

--- TwoBox/ViewController.swift
+++ TwoBox/ViewController.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var pinkBox: UIView!
+    @IBOutlet weak var indigoBox: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
